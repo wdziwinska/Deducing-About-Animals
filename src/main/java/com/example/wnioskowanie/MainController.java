0773bc4fc3542f433a5a12f386e37439f95a3d02
Wioletta Dziwińska <wioletta.dziwinska@student.wat.edu.pl>
2022-12-06 13:31:10 +0100

    Displaying data in GUI from ontology

--- src/main/java/com/example/wnioskowanie/MainController.java
+++ src/main/java/com/example/wnioskowanie/MainController.java
@@ -1,14 +1,65 @@
 package com.example.wnioskowanie;
 
+import com.company.OWLApiController;
+import com.company.Ontology;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 
-public class MainController {
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
     @FXML
-    private Label welcomeText;
+    private Label infoAboutAnimals;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ListView animalsListView;
+
+    @FXML
+    private ChoiceBox choiceBoxGatunek;
+
+    @FXML
+    private ChoiceBox choiceBoxRodzajPozywienia;
+
+    @FXML
+    private CheckBox chbWlosy;
+
+    @FXML
+    private CheckBox chbLuzki;
+
+    @FXML
+    private CheckBox chbPiora;
+
+    @FXML
+    private Button btnWyszukajChoiceBox;
+
+    @FXML
+    private Button btnWyszukajCheckBox;
+
+    Ontology ontology = new Ontology();
+
+   // ArrayList<String> animalsList = ontology.getAnimals();
+
+    String[] anim = {"pies, kot, kaczka"};
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1){
+        try {
+            ontology.ontologyLists();
+        } catch (OWLOntologyCreationException e) {
+            e.printStackTrace();
+        }
+        animalsListView.getItems().addAll(ontology.getAnimals());
+        System.out.println("Animals list view from initialize");
+        System.out.println(ontology.getAnimals());
+//        animalsListView.getItems().addAll(ontology.getAnimalsList());
     }
+
+//    private void animalsListViewData() throws OWLOntologyCreationException {
+//        animalsListView.getItems().addAll(ontology.setAnimalsList());
+//    }
 }
